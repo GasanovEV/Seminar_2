cc72bf4e494a559e684f3becded452cf40019f09
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-20 00:00:16 +0300

    Exercise001_LargerNumber

---
+++ Exercise001_LargerNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+int num = new Random().Next(10,100);
+int a1 = num / 10;
+int a2 = num % 10;
+int max = a1;
+if (max < a2)
+{
+    max = a2;
+}
+Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
