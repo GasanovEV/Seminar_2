7820a7e253a0df60957fa2307a1056fd3daae9a1
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-20 00:38:36 +0300

    Exersice003_multiplicity

---
+++ Exersice003_multiplicity/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число  ");
+string userInput1 = Console.ReadLine() ??"";
+int number1 = int.Parse(userInput1);
+
+Console.WriteLine("Введите второе число  ");
+string userInput2 = Console.ReadLine()??"";
+int number2 = int.Parse(userInput2);
+
+int remains = number1%number2;
+
+if (remains==0)
+{    
+    Console.WriteLine ($"Кратное ");
+}
+else
+{
+    Console.WriteLine ($"{number1} Не кратно {number2} -> Остаток:{remains} ");
+}
+
+        
+
+/*int num = new Random().Next(100,1000);
+int a1 = num / 100;
+int a3 = num % 10;
+int result = a1*10+a3;
+
+Console.WriteLine($"{num} -> {result}");*/
